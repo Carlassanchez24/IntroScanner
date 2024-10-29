d3d2d9c69a7aca0045b30f85e11f6db8aefd83e0
Carlassanchez24 <carlassanchez24@gmail.com>
2024-10-29 10:58:33 +0100

    firts commit

---
+++ src/main/java/org/example/Input.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        String user = "admin";
+        String password = "1234";
+        int contador = 0;
+        boolean autenticado = false;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (contador < 3) {
+            System.out.println("Nombre de usuario: ");
+            String nombre = scanner.nextLine();
+
+
+            System.out.println("Contraseña: ");
+            String contrasenya = scanner.nextLine();
+
+            if (nombre.equals(user) && contrasenya.equals(password)) {
+                System.out.println("Se ha autenticado con éxito");
+                autenticado = true;
+                contador = 3;
+            } else {
+                System.out.println("Usuario y/o contraseña inválido");
+            }
+            contador++;
+        }
+         if (!autenticado){
+            System.out.println("Has excedido el número de intentos");
+        }
+        scanner.close();
+    }
+}
